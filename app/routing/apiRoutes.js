c7ed16dad2ecf57079dcc3d3383c9a50dcc1a67d
alex chalyy <alexchalyy@gmail.com>
2019-04-24 12:28:04 -0400

    comments are fixed

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -28,8 +28,6 @@ module.exports = function(app) {
   // Below code handles when a user submits a form and thus submits data to the server.
   // In each of the below cases, when a user submits form data (a JSON object)
   // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
   // ---------------------------------------------------------------------------
 
   function MatchIndex() {
@@ -63,9 +61,7 @@ module.exports = function(app) {
   //---------------------------------------------------------------
 
   app.post("/api/friends", function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body parsing middleware
+  //  This is api post route definition that posts new "friend" to friends.js array and returns closest match.
       
       friendData.push(req.body);
       for (var i = 0; i < friendData.length; i++) {
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // ==============================================================================
 // DEPENDENCIES
 // Series of npm packages that we will use to give our server useful functionality
+//
+// Written by Alex Chalyy on 4/24/2019.
 // ==============================================================================
 
 var express = require("express");
@@ -30,10 +32,7 @@ app.use(express.json());
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
-// =============================================================================
-// LISTENER
-// The below code effectively "starts" our server
-// =============================================================================
+//  This sets up listener and default route for heroku.
 
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "home.html"));
